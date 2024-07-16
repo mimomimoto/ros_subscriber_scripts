6d4a38a92bc9061489b4306dec873a528b303aa4
mimomimoto <shosei130320@icloud.com>
2024-07-16 08:14:29 +0000

    '7_16'

--- lidar_sub.py
+++ lidar_sub.py
@@ -18,6 +18,25 @@ import pickle
 import csv
 import os
 import sys
+import pandas as pd
+from scipy.spatial import cKDTree
+
+
+ROUGH_DBSCAN_EPS = 0.2
+ROUGH_DBSCAN_MIN_POINTS = 20
+CLUSTER_HEIGHT_THERESH = -0.6
+CLUSTER_POINTS_THERESH = 60
+DOUBLE_CLUSTERING = 10
+
+# ROUGH_DBSCAN_EPS = 0.2
+# ROUGH_DBSCAN_MIN_POINTS = 30
+# DETAIL_DBSCAN_EPS = 0.15
+# CLUSTER_HEIGHT_THERESH = -1.0
+# CLUSTER_POINTS_THERESH = 100
+# DOUBLE_CLUSTERING = 15
+
+SOURCE_PCD = o3d.io.read_point_cloud("/work_space/lidar_data/base/base.pcd")
+
 
 def put_dummy_on_cuda():
     ut = time.time()
@@ -30,62 +49,126 @@ def put_dummy_on_cuda():
     print("put_dummy_on_cuda: ", time.time() - ut)
     print("************************************************")
 
-def cluster(pcd_numpy):
-    ut = time.time()
-    target = o3d.geometry.PointCloud()
-    target.points = o3d.utility.Vector3dVector(pcd_numpy)
-    source = o3d.io.read_point_cloud("/work_space/lidar_data/base/base.pcd")
-    dists = target.compute_point_cloud_distance(source)
-    dists = np.asarray(dists)
-    ind = np.where(dists > 0.1)[0]
-    base_pcd = target.select_by_index(ind, invert=True)
-    object_pcd = target.select_by_index(ind)
-    cl, ind = object_pcd.remove_radius_outlier(nb_points=10, radius=0.5)
-
-    object_noise_1_pcd = object_pcd.select_by_index(ind, invert=True)
-    object_pcd = object_pcd.select_by_index(ind)
+def divide_cluster(pcd, pcd_arrays, thr_points_num):
+    global index1
+    device = o3d.core.Device("CUDA:0")
+    cpu_device = o3d.core.Device("CPU:0")
+    dtype = o3d.core.float64
+    x_y_df = pd.DataFrame(pcd.points,
+                  columns = ["x","y","z"],)
+    x_y_df["z"] = 0
     
+    z_df = pd.DataFrame(pcd.points,
+                  columns = ["x","y","z"],)
     
+    x_y_pcd = o3d.geometry.PointCloud()
+    x_y_pcd.points = o3d.utility.Vector3dVector(x_y_df.to_numpy())
+    thresh_min_points = int(len(x_y_df.index)/DOUBLE_CLUSTERING)
     
+    tmp_x_y_pcd = o3d.t.geometry.PointCloud(device)
+    tmp_x_y_pcd.point.positions = o3d.core.Tensor(np.asarray(x_y_pcd.points), dtype, device)
     
     with o3d.utility.VerbosityContextManager(
         o3d.utility.VerbosityLevel.Debug) as cm:
-        labels = np.array(
-            object_pcd.cluster_dbscan(eps=0.1, min_points=10, print_progress=True))
+        labels_tensor = tmp_x_y_pcd.cluster_dbscan(eps=ROUGH_DBSCAN_EPS, min_points=thresh_min_points, print_progress=False)
+        labels_tensor_cpu = labels_tensor.to(cpu_device)
+        labels = np.array(labels_tensor_cpu.cpu().numpy())
+        
+    tmp_cluster_pcd = o3d.geometry.PointCloud()
     
-    # collect data
+    min_cluster_points = 0
+
+    if labels.size != 0: 
+        for i in range(labels.max() + 1):
+            pc_indices = np.where(labels == i)[0]
+            xyz = np.asarray(z_df.to_numpy())[pc_indices, :]
+            if xyz.T[2].max() > CLUSTER_HEIGHT_THERESH:
+                if len(xyz) >= CLUSTER_POINTS_THERESH:
+                    tmp_pcd = o3d.geometry.PointCloud()
+                    tmp_pcd.points = o3d.utility.Vector3dVector(xyz)
+                    
+                    plane_xyz = np.asarray(x_y_df.to_numpy())[pc_indices, :]
+                    plane_pcd = o3d.geometry.PointCloud()
+                    plane_pcd.points = o3d.utility.Vector3dVector(plane_xyz)
+                    if min_cluster_points == 0:
+                        min_cluster_points = len(tmp_pcd.points)
+                    elif min_cluster_points > len(tmp_pcd.points):
+                        min_cluster_points = len(tmp_pcd.points)
+                    pcd_arrays.append(xyz)
+                    
+                    tmp_cluster_pcd += tmp_pcd
+        
+        dists = pcd.compute_point_cloud_distance(tmp_cluster_pcd)
+        dists = np.asarray(dists)
+        ind = np.where(dists > 0.03 )[0]
+        noise_pcd = pcd.select_by_index(ind)
+        if len(noise_pcd.points) >= min_cluster_points:
+            pcd_arrays = divide_cluster(noise_pcd, pcd_arrays, min_cluster_points)
+    return pcd_arrays
+
+def cluster(pcd_numpy):
+    ut = time.time()
+    cpu_device = o3d.core.Device("CPU:0")
+    device = o3d.core.Device("CUDA:0")
+    dtype = o3d.core.float64
+
+    tmp_target = o3d.t.geometry.PointCloud()
+    tmp_target.point.positions = o3d.core.Tensor(pcd_numpy, dtype, device)
+    tmp_target_numpy = tmp_target.point.positions.cpu().numpy().copy()
+    target = o3d.geometry.PointCloud()
+    target.points = o3d.utility.Vector3dVector(tmp_target_numpy)
+    dists = target.compute_point_cloud_distance(SOURCE_PCD)
+    dists = np.asarray(dists)
+    ind = np.where(dists > 0.1)[0]
+    tmp_object_pcd = target.select_by_index(ind)
+    cl, ind = tmp_object_pcd.remove_radius_outlier(nb_points=10, radius=0.5)
+    tmp_object_pcd = tmp_object_pcd.select_by_index(ind)
+    object_pcd = o3d.t.geometry.PointCloud(device)
+    object_pcd.point.positions = o3d.core.Tensor(np.asarray(tmp_object_pcd.points), dtype, device)
+    
+    back_substruction_time = time.time() - ut
+    
+    ut = time.time()
+
+    with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+        labels_tensor = object_pcd.cluster_dbscan(eps=ROUGH_DBSCAN_EPS, min_points=ROUGH_DBSCAN_MIN_POINTS, print_progress=False)
+        labels_tensor_cpu = labels_tensor.to(cpu_device)
+        labels = np.array(labels_tensor_cpu.cpu().numpy())
+
+
     cluster_pcd = o3d.geometry.PointCloud()
+    
+    pcd_arrays = []
+    
+    if labels.size != 0: 
+        for i in range(labels.max() + 1):
+            pc_indices = np.where(labels == i)[0]
 
-    for i in range(labels.max() + 1):
-        pc_indices = np.where(labels == i)[0]
+            if pc_indices.size > 0:
+                xyz = np.asarray(tmp_object_pcd.points)[pc_indices, :]
+                
+                if xyz.T[2].max() > CLUSTER_HEIGHT_THERESH:
+                    if len(xyz) >= CLUSTER_POINTS_THERESH:
+                        pcd = o3d.geometry.PointCloud()
+                        pcd.points = o3d.utility.Vector3dVector(xyz)
+                        bounding_box = pcd.get_oriented_bounding_box()
+                        size_bounding_box = bounding_box.get_max_bound() - bounding_box.get_min_bound()
+                        ts_size = size_bounding_box[0] * size_bounding_box[1]
+                        if ts_size >= 2:
+                            pcd_arrays = divide_cluster(pcd, pcd_arrays, 0)
+                        else:
+                            pcd_arrays.append(xyz)
+                        cluster_pcd += pcd
+    
+    cluster_time = time.time() - ut
+    
+    # o3d.io.write_point_cloud(f"/work_space/lidar_data/eva/reid/5/{datetime.datetime.now() + datetime.timedelta(hours=9)}.pcd", cluster_pcd)
 
-        if pc_indices.size > 0:
-            xyz = np.asarray(object_pcd.points)[pc_indices, :]
-            
-            if xyz.T[2].max() > -0.7:
-                # print(len(xyz))
-                if len(xyz) >= 50:
-                    pcd = o3d.geometry.PointCloud()
-                    pcd.points = o3d.utility.Vector3dVector(xyz)
-                    cluster_pcd += pcd
-                    # o3d.io.write_point_cloud("../lidar_data/datasets/" + dt_now_str + "/" + dt_now_str + "_" + str(i) + "_data.pcd", pcd)
-            # o3d.io.write_point_cloud("../lidar_data/datasets/" + dt_now_str + "/" + dt_now_str + "_combined_data.pcd", target)
-    
-    # print("************************************************")
-    # print("cl data: ", time.time() - ut, time.time())
-    # print("************************************************")
-    
-    o3d.io.write_point_cloud("/work_space/lidar_data/tmp/t.pcd", cluster_pcd)
-    
-    combined_voxel_numpy = np.asarray(cluster_pcd.points)
-    
-    return combined_voxel_numpy
+    return pcd_arrays, back_substruction_time, cluster_time, len(pcd_arrays)
 
 def callback(point_cloud, args):
     ut = time.time()
-    # print('---------------------------------')
-    # print('st time: ' + str(ut))
-    # print('---------------------------------')
     code = args[0]
     q = args[1]
     
@@ -112,24 +195,11 @@ def callback(point_cloud, args):
     # GPUのメモリにPCDデータを乗せる
     pcd = o3d.t.geometry.PointCloud(device)
     pcd.point.positions = o3d.core.Tensor(points, dtype, device)
-    
-    print(pcd)
-    print(pcd.is_cuda)
-    
     # ボクセル化(1cm)
     voxel_pcd = pcd.voxel_down_sample(voxel_size=0.05)
-    
-    # print("************************************************")
-    # print(code, " voxel: ", time.time() - ut, time.time())
-    # print("************************************************")
-    
-    
+
     # 点群データの回転
     voxel_pcd_rotated = voxel_pcd.transform(np.array(config[code]))
-    # print("************************************************")
-    # print(pcd.point)
-    # print(code, " rotate: ", time.time() - ut, time.time())
-    # print("************************************************")
     
     if q.full():
         print('*******************************************************************')
@@ -139,10 +209,9 @@ def callback(point_cloud, args):
         print('*******************************************************************')
     
     if q.qsize() >= 1:
-        print('clear que')
+        # print('clear que')
         q.get()
     q.put(voxel_pcd_rotated)
-    # print('qsize: ', q.qsize())
     
     print("************************************************")
     print(code, " que: ", time.time() - ut, time.time())
@@ -154,7 +223,7 @@ def callback(point_cloud, args):
         os.remove(file)
 
     o3d.t.io.write_point_cloud("/work_space/lidar_data/" + code + "/" + dt_now_str + ".pcd", pcd)
-    print("save " + code + " data")
+    # print("save " + code + " data")
 
     
 def connect_ros(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201):
@@ -172,71 +241,59 @@ def combine_pcd(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL
     sock_sender.initialize_sender('192.168.50.30', 49220)
     send_data_time =  time.time()
     while 1:
-        try:
+        # try:
             
-            pcd_3JEDKBS001G9601 = q_3JEDKBS001G9601.get()
+        pcd_3JEDKBS001G9601 = q_3JEDKBS001G9601.get()
 
-            pcd_3JEDKC50014U011 = q_3JEDKC50014U011.get()
+        pcd_3JEDKC50014U011 = q_3JEDKC50014U011.get()
 
-            pcd_3JEDL3N0015X621 = q_3JEDL3N0015X621.get()
-            
-            pcd_3JEDL76001L4201 = q_3JEDL76001L4201.get()
-            
-            ut = time.time()
+        pcd_3JEDL3N0015X621 = q_3JEDL3N0015X621.get()
+        
+        pcd_3JEDL76001L4201 = q_3JEDL76001L4201.get()
+        
+        ut = time.time()
 
 
-            combined_pcd = pcd_3JEDKBS001G9601 + pcd_3JEDKC50014U011 + pcd_3JEDL3N0015X621 + pcd_3JEDL76001L4201
-            
-            combined_voxel_pcd = combined_pcd.voxel_down_sample(voxel_size=0.05)
-            
-            now_dt = datetime.datetime.now()
-            if 18 == now_dt.hour and 5 > now_dt.minute and now_dt.minute > 0:
-                o3d.t.io.write_point_cloud("/work_space/lidar_data/base/base.pcd", combined_voxel_pcd)
-            
-            # print("************************************************")
-            # print("combined data: ", time.time() - ut, time.time())
-            # print("************************************************")
-            # ut = time.time()
+        combined_pcd = pcd_3JEDKBS001G9601 + pcd_3JEDKC50014U011 + pcd_3JEDL3N0015X621 + pcd_3JEDL76001L4201
+        
+        combined_voxel_pcd = combined_pcd.voxel_down_sample(voxel_size=0.05)
 
-            combined_voxel_numpy = combined_voxel_pcd.point.positions.cpu().numpy().copy()
-            # ft = lt
-            # lt = time.time()
-            
-            # print("************************************************")
-            # print("to cpu: ", time.time() - ut, time.time())
-            # print("************************************************")
-            ut = time.time()
-            
-            combined_voxel_numpy = cluster(combined_voxel_numpy)
-            ut = time.time()
-            
-            # with open('/work_space/tmp.csv', 'a') as f:
-            #     writer = csv.writer(f)
-            #     writer.writerow([str(time.time())])
-            data = pickle.dumps(combined_voxel_numpy)
-            
-            message_size = struct.pack("I", len(data))
-            sock_sender.socket.sendall(message_size + data)
-            send_data_time = time.time()
-            print("************************************************")
-            print("send data: ", time.time() - ut, time.time())
-            print("************************************************")
-            ut = time.time()
-            
-            
-            dt_now = datetime.datetime.now()
-            dt_now_str = dt_now.strftime('%Y_%m_%d_%H_%M_%S')
-            # for file in glob.glob("/work_space/lidar_data/combined_pcd" + "/*.pcd", recursive=True):
-            #     os.remove(file)
+        now_dt = datetime.datetime.now()
+        if 18 == now_dt.hour and 5 > now_dt.minute and now_dt.minute > 0:
+            o3d.t.io.write_point_cloud("/work_space/lidar_data/base/base.pcd", combined_voxel_pcd)
 
-            o3d.t.io.write_point_cloud("/work_space/lidar_data/combined_pcd/combined.pcd", combined_voxel_pcd)
+        combined_voxel_numpy = combined_voxel_pcd.point.positions.cpu().numpy().copy()
+        
+        combined_data_time = time.time() - ut
+        
+        
+        combined_voxel_numpy, back_substruction_time, cluster_time, cluster_size = cluster(combined_voxel_numpy)
+        
+        # with open('/work_space/lidar_data/edge_process.csv', 'a') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow([datetime.datetime.now() + datetime.timedelta(hours=9), cluster_size, combined_data_time, back_substruction_time, cluster_time])
+
+        data = pickle.dumps(combined_voxel_numpy)
+        
+        
+        
+        message_size = struct.pack("I", len(data))
+        sock_sender.socket.sendall(message_size + data)
+        send_data_time = time.time()
+        print("************************************************")
+        print("send data: ", time.time() - ut)
+        print("************************************************")
+        
+        
+        dt_now = datetime.datetime.now()
+        dt_now_str = dt_now.strftime('%Y_%m_%d_%H_%M_%S')
+
+        # o3d.t.io.write_point_cloud(f"/work_space/lidar_data/eva/5/{datetime.datetime.now() + datetime.timedelta(hours=9)}.pcd", combined_voxel_pcd)
+        o3d.t.io.write_point_cloud("/work_space/lidar_data/combined_pcd/combined.pcd", combined_voxel_pcd)
+        print("save combined data")
+        # except:
             
-                
-            # o3d.t.io.write_point_cloud("/work_space/lidar_data/combined_pcd/" + dt_now_str + ".pcd", combined_voxel_pcd)
-            print("save combined data")
-        except:
-            
-            continue
+        #     continue
         
         
 
@@ -264,4 +321,4 @@ def main():
 if __name__ == '__main__':
     main()
     
-    
+     

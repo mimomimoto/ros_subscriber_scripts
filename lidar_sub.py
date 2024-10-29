74fb77b6138abba33441016162fa4d02f8b90974
mimomimoto <99246880+mimomimoto@users.noreply.github.com>
2024-10-29 21:08:13 +0900

    Update lidar_sub.py

--- lidar_sub.py
+++ lidar_sub.py
@@ -20,13 +20,30 @@ import os
 import sys
 import pandas as pd
 from scipy.spatial import cKDTree
+import iperf3
+import copy
 
-
-ROUGH_DBSCAN_EPS = 0.2
-ROUGH_DBSCAN_MIN_POINTS = 20
-CLUSTER_HEIGHT_THERESH = -0.6
+# lab
+ROUGH_DBSCAN_EPS = 0.1
+ROUGH_DBSCAN_MIN_POINTS = 10
+CLUSTER_MIN_HEIGHT_THERESH = -0.6
+CLUSTER_MAX_HEIGHT_THERESH = 1
 CLUSTER_POINTS_THERESH = 60
-DOUBLE_CLUSTERING = 10
+CLUSTER_BOX_SIZE_THERESH = 2
+STRICT_DBSCAN_EPS = 0.2
+STRICT_DBSCAN_MIN_POINTS_DIV = 10
+
+# splab
+# ROUGH_DBSCAN_EPS = 0.1
+# ROUGH_DBSCAN_MIN_POINTS = 10
+# CLUSTER_MIN_HEIGHT_THERESH = -3
+# CLUSTER_MAX_HEIGHT_THERESH = 0.5
+# CLUSTER_POINTS_THERESH = 200
+# CLUSTER_BOX_SIZE_THERESH = 2
+# STRICT_DBSCAN_EPS = 0.2
+# STRICT_DBSCAN_MIN_POINTS_DIV = 10
+
+
 
 # ROUGH_DBSCAN_EPS = 0.2
 # ROUGH_DBSCAN_MIN_POINTS = 30
@@ -37,6 +54,7 @@ DOUBLE_CLUSTERING = 10
 
 SOURCE_PCD = o3d.io.read_point_cloud("/work_space/lidar_data/base/base.pcd")
 
+SERVER_FLAG = False
 
 def put_dummy_on_cuda():
     ut = time.time()
@@ -63,14 +81,14 @@ def divide_cluster(pcd, pcd_arrays, thr_points_num):
     
     x_y_pcd = o3d.geometry.PointCloud()
     x_y_pcd.points = o3d.utility.Vector3dVector(x_y_df.to_numpy())
-    thresh_min_points = int(len(x_y_df.index)/DOUBLE_CLUSTERING)
+    thresh_min_points = int(len(x_y_df.index)/STRICT_DBSCAN_MIN_POINTS_DIV)
     
     tmp_x_y_pcd = o3d.t.geometry.PointCloud(device)
     tmp_x_y_pcd.point.positions = o3d.core.Tensor(np.asarray(x_y_pcd.points), dtype, device)
     
     with o3d.utility.VerbosityContextManager(
         o3d.utility.VerbosityLevel.Debug) as cm:
-        labels_tensor = tmp_x_y_pcd.cluster_dbscan(eps=ROUGH_DBSCAN_EPS, min_points=thresh_min_points, print_progress=False)
+        labels_tensor = tmp_x_y_pcd.cluster_dbscan(eps=STRICT_DBSCAN_EPS, min_points=thresh_min_points, print_progress=False)
         labels_tensor_cpu = labels_tensor.to(cpu_device)
         labels = np.array(labels_tensor_cpu.cpu().numpy())
         
@@ -82,7 +100,7 @@ def divide_cluster(pcd, pcd_arrays, thr_points_num):
         for i in range(labels.max() + 1):
             pc_indices = np.where(labels == i)[0]
             xyz = np.asarray(z_df.to_numpy())[pc_indices, :]
-            if xyz.T[2].max() > CLUSTER_HEIGHT_THERESH:
+            if CLUSTER_MAX_HEIGHT_THERESH > xyz.T[2].max() > CLUSTER_MIN_HEIGHT_THERESH:
                 if len(xyz) >= CLUSTER_POINTS_THERESH:
                     tmp_pcd = o3d.geometry.PointCloud()
                     tmp_pcd.points = o3d.utility.Vector3dVector(xyz)
@@ -148,14 +166,14 @@ def cluster(pcd_numpy):
             if pc_indices.size > 0:
                 xyz = np.asarray(tmp_object_pcd.points)[pc_indices, :]
                 
-                if xyz.T[2].max() > CLUSTER_HEIGHT_THERESH:
+                if CLUSTER_MAX_HEIGHT_THERESH > xyz.T[2].max() > CLUSTER_MIN_HEIGHT_THERESH:
                     if len(xyz) >= CLUSTER_POINTS_THERESH:
                         pcd = o3d.geometry.PointCloud()
                         pcd.points = o3d.utility.Vector3dVector(xyz)
                         bounding_box = pcd.get_oriented_bounding_box()
                         size_bounding_box = bounding_box.get_max_bound() - bounding_box.get_min_bound()
                         ts_size = size_bounding_box[0] * size_bounding_box[1]
-                        if ts_size >= 2:
+                        if ts_size >= CLUSTER_BOX_SIZE_THERESH:
                             pcd_arrays = divide_cluster(pcd, pcd_arrays, 0)
                         else:
                             pcd_arrays.append(xyz)
@@ -202,11 +220,11 @@ def callback(point_cloud, args):
     voxel_pcd_rotated = voxel_pcd.transform(np.array(config[code]))
     
     if q.full():
-        print('*******************************************************************')
-        print('*******************************************************************')
+        # print('*******************************************************************')
+        # print('*******************************************************************')
         print('full error')
-        print('*******************************************************************')
-        print('*******************************************************************')
+        # print('*******************************************************************')
+        # print('*******************************************************************')
     
     if q.qsize() >= 1:
         # print('clear que')
@@ -235,13 +253,29 @@ def connect_ros(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL
     rospy.Subscriber("/livox/lidar_3JEDL76001L4201", PointCloud2, callback, ("3JEDL76001L4201", q_3JEDL76001L4201))
     rospy.spin()
     
-def combine_pcd(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201):
+def combine_pcd(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201, q_band_width, q_server_back_substruction_time, q_server_cluster_time, q_server_after_cluster_size):
     global REBOOT_FLAG
+    global SERVER_FLAG
     sock_sender = SocketNumpyArray()
     sock_sender.initialize_sender('192.168.50.30', 49220)
     send_data_time =  time.time()
+    band_with = 0
+    server_back_substruction_time = 0
+    server_cluster_time = 0
+    server_cluster_size = 0
+    
     while 1:
         # try:
+        if q_band_width.qsize() >= 1:
+            band_with = q_band_width.get()
+        if q_server_back_substruction_time.qsize() >= 1:
+            server_back_substruction_time = q_server_back_substruction_time.get()
+        if q_server_cluster_time.qsize() >= 1:
+            server_cluster_time = q_server_cluster_time.get()
+        if q_server_after_cluster_size.qsize() >= 1:
+            server_cluster_size = q_server_after_cluster_size.get()
+        
+        print('band_with: ', band_with)
             
         pcd_3JEDKBS001G9601 = q_3JEDKBS001G9601.get()
 
@@ -261,28 +295,71 @@ def combine_pcd(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL
         now_dt = datetime.datetime.now()
         if 18 == now_dt.hour and 5 > now_dt.minute and now_dt.minute > 0:
             o3d.t.io.write_point_cloud("/work_space/lidar_data/base/base.pcd", combined_voxel_pcd)
-
+            
+        # bb = o3d.t.geometry.AxisAlignedBoundingBox(
+        #     np.array([[0],[-6],[-100]]),
+        #     np.array([[7],[1],[10]]),
+        # )
+        
         combined_voxel_numpy = combined_voxel_pcd.point.positions.cpu().numpy().copy()
-        
-        combined_data_time = time.time() - ut
-        
-        
-        combined_voxel_numpy, back_substruction_time, cluster_time, cluster_size = cluster(combined_voxel_numpy)
-        
-        # with open('/work_space/lidar_data/edge_process.csv', 'a') as f:
-        #     writer = csv.writer(f)
-        #     writer.writerow([datetime.datetime.now() + datetime.timedelta(hours=9), cluster_size, combined_data_time, back_substruction_time, cluster_time])
+        before_data = pickle.dumps(combined_voxel_numpy)
+        before_data_len = len(before_data)/1024/1024
 
-        data = pickle.dumps(combined_voxel_numpy)
+
+        # combined_voxel_pcd = combined_voxel_pcd.crop(bb)
+        if SERVER_FLAG == True:
+            be_pack_data = time.time()
+            flag = struct.pack("I", 1)
+            message_size = struct.pack("I", len(before_data))
+            send_time = struct.pack("d", time.time())
+            print('time: ', time.time())
+            sock_sender.socket.sendall(flag + message_size + send_time + before_data)
+            print('send data: ', time.time() - be_pack_data)
+            print('left_time: ', (server_back_substruction_time + server_cluster_time))
+            print('right_time: ', (before_data_len + server_cluster_size)*8/band_with)
+            
+            if band_with != 0 and server_cluster_size != 0 and server_back_substruction_time != 0 and server_cluster_time != 0:
+                if (server_back_substruction_time + server_cluster_time) < (before_data_len + server_cluster_size)*8/band_with:
+                    SERVER_FLAG = False
+            with open('/work_space/lidar_data/process_change.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([datetime.datetime.now() + datetime.timedelta(hours=9), band_with, server_back_substruction_time, server_cluster_time, 1])
+            
+
+        else:
+            combined_voxel_numpy, back_substruction_time, cluster_time, cluster_size = cluster(combined_voxel_numpy)
         
-        
-        
-        message_size = struct.pack("I", len(data))
-        sock_sender.socket.sendall(message_size + data)
+            # with open('/work_space/lidar_data/edge_process.csv', 'a') as f:
+            #     writer = csv.writer(f)
+            #     writer.writerow([datetime.datetime.now() + datetime.timedelta(hours=9), cluster_size, combined_data_time, back_substruction_time, cluster_time])
+            
+            be_pack_data = time.time()
+
+            data = pickle.dumps(combined_voxel_numpy)
+            after_data_len = len(data)/1024/1024
+            
+            flag = struct.pack("I", 0)
+            message_size = struct.pack("I", len(data))
+            send_time = struct.pack("d", time.time())
+            print('time: ', time.time())
+            sock_sender.socket.sendall(flag + message_size + send_time + data)
+            
+            print('send data: ', time.time() - be_pack_data)
+            
+            if band_with != 0:
+                print('left_time: ', (back_substruction_time + cluster_time))
+                print('right_time: ', (before_data_len + after_data_len)*8/band_with)
+                if (back_substruction_time + cluster_time)/2 > (before_data_len + after_data_len)*8/band_with:
+                    SERVER_FLAG = True
+            with open('/work_space/lidar_data/process_change.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([datetime.datetime.now() + datetime.timedelta(hours=9), band_with, back_substruction_time, cluster_time, 0])
+
         send_data_time = time.time()
-        print("************************************************")
-        print("send data: ", time.time() - ut)
-        print("************************************************")
+        
+        # print("************************************************")
+        # print("send data: ", time.time() - ut)
+        # print("************************************************")
         
         
         dt_now = datetime.datetime.now()
@@ -291,10 +368,64 @@ def combine_pcd(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL
         # o3d.t.io.write_point_cloud(f"/work_space/lidar_data/eva/5/{datetime.datetime.now() + datetime.timedelta(hours=9)}.pcd", combined_voxel_pcd)
         o3d.t.io.write_point_cloud("/work_space/lidar_data/combined_pcd/combined.pcd", combined_voxel_pcd)
         print("save combined data")
+            
         # except:
             
         #     continue
+
+def iperf_client(q_band_width):
+    while 1:
+        client = iperf3.Client()
+        client.duration = 5
+        client.server_hostname = '192.168.50.30'
+        client.port = 5201
+        client.bytes = 10 * 1024 * 1024
+        result = client.run()
+        q_band_width.put(result.sent_Mbps)
+        # print('sent_Mbps: ', result.sent_Mbps)
+        client = None
+
+        time.sleep(5)
         
+def share_processing_time_server(q_server_back_substruction_time, q_server_cluster_time, q_server_after_cluster_size):
+    sock_receiver = SocketNumpyArray()
+    sock_receiver.address = '192.168.107.50'
+    sock_receiver.port = 49227
+    sock_receiver.socket.bind((sock_receiver.address, sock_receiver.port))
+    print('Socket bind complete')
+    sock_receiver.socket.listen(10)
+    while True:
+        sock_cl = copy.copy(sock_receiver)
+        sock_cl.conn, addr = sock_receiver.socket.accept()
+
+        print('Socket now listening')
+        sock_cl.payload_size = struct.calcsize("I")  # CHANGED
+        sock_cl.data = b''
+        while True:
+        
+            while len(sock_cl.data) < 4:
+                sock_cl.data += sock_cl.conn.recv(4096)
+
+            packed_msg_size = sock_cl.data[:4]
+            sock_cl.data = sock_cl.data[4:]
+            msg_size = struct.unpack("I", packed_msg_size)[0]
+
+            while len(sock_cl.data) < msg_size:
+                sock_cl.data += sock_cl.conn.recv(4096)
+
+            frame_data = sock_cl.data[:msg_size]
+            sock_cl.data = sock_cl.data[msg_size:]
+            
+            
+
+            server_processing_time = pickle.loads(frame_data)
+            
+            print('server_processing_time: ', server_processing_time)
+            
+            q_server_back_substruction_time.put(server_processing_time[0])
+            q_server_cluster_time.put(server_processing_time[1])
+            q_server_after_cluster_size.put(server_processing_time[2])
+
         
 
 def main():
@@ -307,15 +438,31 @@ def main():
     q_3JEDKC50014U011 = manager.Queue()
     q_3JEDL3N0015X621 = manager.Queue()
     q_3JEDL76001L4201 = manager.Queue()
+    
+    q_server_back_substruction_time = mp.Queue()
+    q_server_cluster_time = mp.Queue()
+    q_server_after_cluster_size = mp.Queue()
+    q_band_width = mp.Queue()
+    
+    
 
     p_connect_ros = mp.Process(target=connect_ros, args=(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201))
     p_connect_ros.start()
     
-    p_combine_pcd = mp.Process(target=combine_pcd, args=(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201))
+    p_combine_pcd = mp.Process(target=combine_pcd, args=(q_3JEDKBS001G9601, q_3JEDKC50014U011, q_3JEDL3N0015X621, q_3JEDL76001L4201, q_band_width, q_server_back_substruction_time, q_server_cluster_time, q_server_after_cluster_size))
     p_combine_pcd.start()
+    
+    p_iperf_client = mp.Process(target=iperf_client,args=(q_band_width,))
+    p_iperf_client.start()
+    
+    p_share_processing_time_server = mp.Process(target=share_processing_time_server, args=(q_server_back_substruction_time, q_server_cluster_time, q_server_after_cluster_size))
+    p_share_processing_time_server.start()
     
     p_connect_ros.join()
     p_combine_pcd.join()
+    p_iperf_client.join()
+    p_share_processing_time_server.join()
+    
 
 
 if __name__ == '__main__':
